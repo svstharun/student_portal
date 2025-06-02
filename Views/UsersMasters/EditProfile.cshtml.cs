3c30f16824644275127400392bfa3301c87ca9e1
Tharun Sankarsetty V S <153394012+svstharun@users.noreply.github.com>
2025-06-02 10:17:46 +0530

    Add files via upload

---
+++ Views/UsersMasters/EditProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentPortalDBFirst.Views.UsersMasters
+{
+    public class EditProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
